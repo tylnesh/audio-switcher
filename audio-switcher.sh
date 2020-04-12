fdf56b3f4c56af371c343d2f9d7830e4db3a4a3a
tylnesh <kohutek.michal@gmail.com>
2020-04-12 17:16:23 +0200

    merged two scripts into one

--- audio-switcher.sh
+++ audio-switcher.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+if [ "$SNAP_ARCH" == "amd64" ]; then
+  ARCH="x86_64-linux-gnu"
+elif [ "$SNAP_ARCH" == "armhf" ]; then
+  ARCH="arm-linux-gnueabihf"
+elif [ "$SNAP_ARCH" == "arm64" ]; then
+  ARCH="aarch64-linux-gnu"
+else
+  ARCH="$SNAP_ARCH-linux-gnu"
+fi
+
+# Pulseaudio export
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SNAP/usr/lib/$ARCH/pulseaudio
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SNAP/lib/$ARCH
+
 SINKS_COUNT=`pactl list sinks short | awk '{print $1}' | wc -l`
 SINKS=(`pactl list sinks short | awk '{print $1}'`)
 RUNNING_SINK=(`pactl list sinks short | grep RUNNING | awk '{print $1}'`)
